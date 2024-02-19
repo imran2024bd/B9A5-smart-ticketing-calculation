9ad2e5138c0977a37086edfdbae9136379243b14
imran2024bd <imranitbd34@gmail.com>
2024-02-19 23:56:38 +0600

    seat counting section done

--- cal.js
+++ cal.js
@@ -15,17 +15,30 @@ const selectionsSeatNumber = [];
 for (const selection of selections){
 // console.log(selections.target);
 selection.addEventListener('click', function (e) {
-const SelectedSeat = e.target.innerText;
-if (!selectionsSeatNumber.includes(SelectedSeat)) {
+const SelectedSeatNumber = e.target.innerText;
+if (!selectionsSeatNumber.includes(SelectedSeatNumber)) {
     if (selectionsSeatNumber.length >= 4) {
         alert(' Already Exceeds your Limit 4 Seats')
     }
-    selectionsSeatNumber.push(SelectedSeat);
+    selectionsSeatNumber.push(SelectedSeatNumber);
     e.target.classList.toggle('bg-themecolor');
-    countingSeat=selectionsSeatNumber.length;
+    countingSeat = selectionsSeatNumber.length;
+    setInnerText('counting-seatNumber' , countingSeat );
+    remainingSeat = 40 - selectionsSeatNumber.length ;
+    setInnerText('remaining-seatNumber' , remainingSeat );
+
+    
     
 
 }
     
 })
+}
+
+
+// set inner Text Utility Function
+
+function setInnerText(id , value) {
+    const newValue = document.getElementById(id);
+    newValue.innerText = value;
 }
