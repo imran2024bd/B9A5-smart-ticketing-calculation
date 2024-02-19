2ad4a37b98ac90b06077850302fdd931568f18f1
imran2024bd <imranitbd34@gmail.com>
2024-02-19 16:55:01 +0600

    calculation of seats working

--- cal.js
+++ cal.js
@@ -3,6 +3,11 @@ let countingSeat = parseInt(document.getElementById('counting-seatNumber').inner
 let remainingSeat = parseInt(document.getElementById('remaining-seatNumber').innerText);
 const perTicketPrice = parseInt(document.getElementById('per-ticket-price').innerText);
 
+// console.log('selected-seat' , selected-seat);
+// console.log('perTicketPrice' , perTicketPrice);
+// console.log('countingSeat' , countingSeat);
+// console.log('remaining-seatNumber' , remaining-seatNumber);
+
 let initialTicketPrice = 0;
 
 const selectionsSeatNumber = [];
@@ -10,6 +15,17 @@ const selectionsSeatNumber = [];
 for (const selection of selections){
 // console.log(selections.target);
 selection.addEventListener('click', function (e) {
+const SelectedSeat = e.target.innerText;
+if (!selectionsSeatNumber.includes(SelectedSeat)) {
+    if (selectionsSeatNumber.length >= 4) {
+        alert(' Already Exceeds your Limit 4 Seats')
+    }
+    selectionsSeatNumber.push(SelectedSeat);
+    e.target.classList.toggle('bg-themecolor');
+    countingSeat=selectionsSeatNumber.length;
+    
+
+}
     
 })
 }
