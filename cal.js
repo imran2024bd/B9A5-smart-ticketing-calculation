9a08f2aedddd3f29110a7b13f2e14abf778107c4
imran2024bd <imranitbd34@gmail.com>
2024-02-19 16:02:08 +0600

    Calculation Initial part start

---
+++ cal.js
@@ -0,0 +1,15 @@
+const selections = document.getElementsByClassName('selected-seat');
+let countingSeat = parseInt(document.getElementById('counting-seatNumber').innerText);
+let remainingSeat = parseInt(document.getElementById('remaining-seatNumber').innerText);
+const perTicketPrice = parseInt(document.getElementById('per-ticket-price').innerText);
+
+let initialTicketPrice = 0;
+
+const selectionsSeatNumber = [];
+
+for (const selection of selections){
+// console.log(selections.target);
+selection.addEventListener('click', function (e) {
+    
+})
+}
